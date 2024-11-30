ceb8deff76398a6f62fd7c0d7c15de0afbcead89
Omer Haim Israeli <omeri@elementor.com>
2024-11-30 18:34:12 +0200

    chore: renaming

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, it, vitest } from 'vitest';
-import { rezpond } from '../rezpond';
+import { rezpond } from '../index';
 
 describe('Rezpond', () => {
 	describe('render', () => {
